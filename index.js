54977a5640352392b622cbdc0bb787ebde04876c
Charlie Warner <charlie@omnium-events.com>
2018-01-24 03:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var tests = require("./tests.js")
 var name = "Joe"
-var height = "74"
+var height = "70"
 
 // Don't worry about this
 module.exports = { name, height
+}
+
+if (tests.name === "Joe") {
+  console.log("The name is correct")
+} else { console.log("incorrect") }
+
+if (test.height ==== 74) {
+  console.log("Height is correct")
 }
