77fa3404974f217bf2bbc637bb1a45c73748dfcf
Charlie Warner <charlie@omnium-events.com>
2018-01-24 02:48:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var name = "Joe"
 var height = "74"
-name = "Susan"
 
 // Don't worry about this
 module.exports = { name, height
-}
-
-if (name === "Susan") {
-  console.log("This is correct");
-} else {
-  console.log("Expected name to be Susan");
 }
